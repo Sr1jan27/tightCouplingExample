7c2fc15ad51ee7e0cad708df06552f5379ac2891
Sr1jan27 <singhsrijan809@gmail.com>
2024-11-14 09:38:20 +0530

    tight coupling

--- src/main/java/com/tight/coupling/UserManager.java
+++ src/main/java/com/tight/coupling/UserManager.java
@@ -1,4 +1,9 @@
 package com.tight.coupling;
 
 public class UserManager {
+
+    public UserDatabase userDatabase = new UserDatabase();
+    public String getUserInfo(){
+        return userDatabase.getUserDetails();
+    }
 }
