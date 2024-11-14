3f9f9b613d02e9d8c39630db3e24f90859572871
Sr1jan27 <singhsrijan809@gmail.com>
2024-11-15 00:56:12 +0530

    checking bean working

--- src/main/java/car/example/bean/MyBean.java
+++ src/main/java/car/example/bean/MyBean.java
@@ -4,6 +4,10 @@ public class MyBean {
 
     private String message;
 
+    public String getMessage() {
+        return message;
+    }
+
     public void setMessage(String message) {
         this.message = message;
     }
